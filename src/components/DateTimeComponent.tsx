65e0da48cec93c74764b323da774c079772ffaf1
AbdelrahmanGamal <abdelrhman.gamal53@gmail.com>
2024-08-13 19:17:31 +0300

    fix the time and date render

--- src/components/DateTimeComponent.tsx
+++ src/components/DateTimeComponent.tsx
@@ -27,9 +27,9 @@ const DateTimeComponent = () => {
 
   return (
     <div className="px-4 mb-4">
-      <p className="text-[clamp(16px,3vw,48px)] font-black">{timeString}</p>
+      <p className="text-[clamp(16px,3vw,48px)] font-black">{time}</p>
       <p className="text-[clamp(12px,2vw,24px)] text-blue-2 font-medium">
-        {dateString}
+        {date}
       </p>
     </div>
   );
